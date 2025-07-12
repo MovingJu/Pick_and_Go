c95acc783060a58f0ca80ff86d30a4ff74f208b8
MovingJu <baramishe@gmail.com>
2025-07-12 19:41:13 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from pick-and-go!")
+from fastapi import FastAPI
+
+APP = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@APP.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@APP.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
