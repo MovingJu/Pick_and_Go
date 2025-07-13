d9c2087d2c87f2f17ede4f3d57314b5d80b29f44
MovingJu <baramishe@gmail.com>
2025-07-13 13:12:50 +0900

    database setting

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-APP = FastAPI()
+app = FastAPI()
 
 
-@APP.get("/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@APP.get("/items/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
