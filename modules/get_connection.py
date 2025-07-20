1b78374eaf701eb4c87f844bda581e7688142ead
MovingJu <baramishe@gmail.com>
2025-07-20 16:20:42 +0900

    db run with queries

--- modules/get_connection.py
+++ modules/get_connection.py
@@ -7,7 +7,7 @@ load_dotenv()
 async def get_connection():
     conn = await aiomysql.connect(
         host = os.getenv("DB_HOST"),
-        port = os.getenv("DB_PORT"),
+        port = int(os.getenv("DB_PORT")),
         user = os.getenv("DB_USER"),
         password = os.getenv("DB_PW"), 
         db = os.getenv("DB_NAME"), 
--- routes/db_test.py
+++ routes/db_test.py
@@ -7,12 +7,14 @@ router = APIRouter(
     tags=["testing db functions"]
 )
 
-@router.get("/insert/{query}")
+@router.get("/select/{query}")
 async def read_item(query: str):
     conn = await modules.get_connection()
 
     async with conn.cursor(aiomysql.DictCursor) as cur:
         await cur.execute(query)
         results = await cur.fetchall()
+
+    conn.close()
 
     return {"query": query, "result" : results}
