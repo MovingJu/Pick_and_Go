3ed4dee00f901fdaa20558bc3205c1f860c3a2a5
MovingJu <baramishe@gmail.com>
2025-07-27 16:40:44 +0900

    add related locations functionality

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,3 +4,4 @@ from .schema import User_info, User_tour_loc, DB_TABLE_SETUP_QUERY
 from .get_connection import get_connection
 from .Manage import Setup, Manage
 from .TourAPI import Url, TourAPI
+from .Picked_sigungu import Picked_sigungu
