579e51963a1723732c4e5666517be620fb3b34a9
MovingJu <baramishe@gmail.com>
2025-08-05 19:38:44 +0900

    remove errors

--- routes/test.py
+++ routes/test.py
@@ -13,11 +13,9 @@ async def image_compare(image1_link: str, image2_link: str):
 
     features = modules.Image_comparison.extract_features(
         image1_link,
-        modules.Image_comparison.get_feature_extractor()
     )
     features1 = modules.Image_comparison.extract_features(
         image2_link,
-        modules.Image_comparison.get_feature_extractor()
     )
 
     return {"result" : float(modules.Image_comparison.cosine_similarity(
