9565238482c01faeb325c5833b54e11a9fb81c91
MovingJu <baramishe@gmail.com>
2025-08-07 14:50:13 +0900

    semi filter

--- modules/Picked_sigungu.py
+++ modules/Picked_sigungu.py
@@ -26,32 +26,8 @@ class Picked_sigungu():
             #urls.append(modules.Url("areaBasedList2", lDongRegnCd=loc[0], lDongSignguCd=loc[1], numOfRows=30, pageNo=1, arrange='C')) # 서버 부하 줄이기용
         tour = await modules.TourAPI.create(*urls)
         data = await tour.fetch_url()
-        filtered_data={'totalCount':0, 'items':[]}
-
-        for i in data['items']: # type: ignore
-            if(i['lclsSystm1']=='AC'): #숙소
-                continue
-            elif(i['lclsSystm1']=='FD'): #식당
-                continue
-            elif(i['lclsSystm2']=='SH04'): #마트
-                continue
-            elif(i['lclsSystm3']=='NA020600'): #염전 NA020600
-                continue
-            elif(i['lclsSystm3']=='C0117000'): #맛코스 C0117000
-                continue
-            elif(i['lclsSystm3']=='VE060200'): #영화관 VE060200
-                continue
-            elif(i['lclsSystm3']=='VE090600'): #학교 VE090600
-                continue
-            elif(i['lclsSystm3']=='SH030100'): #대형마트 SH030100
-                continue
-            elif(i['lclsSystm3']=='SH050200'): #안경점
-                continue
-            else:
-                filtered_data['items'].append(i)
         
-        filtered_data['totalCount']=len(filtered_data['items'])
-        return filtered_data
+        return data
 
 if __name__ == "__main__":
     async def main():
