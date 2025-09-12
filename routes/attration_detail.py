8228ca6696e2e53df265cdead3080ff1b141e515
MovingJu <baramishe@gmail.com>
2025-09-12 23:05:21 +0900

    엄준식

--- routes/attration_detail.py
+++ routes/attration_detail.py
@@ -1,22 +1,28 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+import modules
 
 
 router = APIRouter(
-    prefix="/info",
+    prefix="/detail",
     tags=["관광지 세부 정보 제공 엔드포인트임."]
 )
 
 
-class InputData(BaseModel):
-    contentId: str
-
-
-@router.get("/")
-async def get_info(item: InputData):
+@router.get("/{contentId}")
+async def get_info(contentId: str):
 
     # 개빠르게 조회하는 기능
+    attraction_detail_url = modules.Url(
+            "detailCommon2", 
+            contentId="126128",
+            numOfRows=1
+        )
+    api_client = await modules.TourAPI.create(attraction_detail_url)
+    data = await api_client.fetch_async()
+
+    print(data)
+
     # 조회한 데이터 함치는 기능
 
 
-    return
+    return {"Message" : contentId}
