fa9ac95156aaf49da576e4e7a0d31d32385bfb21
unknown <makesense1227@khu.ac.kr>
2025-09-13 09:27:20 +0900

    detail, detail_pet

--- routes/attraction_detail.py
+++ routes/attraction_detail.py
@@ -9,13 +9,29 @@ router = APIRouter(
 @router.get("/{contentId}")
 async def get_info(contentId: str):
 
-    detail_url_base_list=['detailCommon2','detailIntro2','detailInfo2','detailPetTour2']
+    #detail_url_base_list=['detailCommon2','detailIntro2','detailInfo2','detailPetTour2']
+    detail_url_base_list=['detailCommon2','detailPetTour2']
     detail_url_list=[]
     for i in detail_url_base_list:
         detail_url_list.append(modules.Url(i, contentId=contentId))
     api_client = await modules.TourAPI.create(*detail_url_list)
-    data = await api_client.fetch_async()
+    fetched_data = await api_client.fetch_async()
 
-    
+    extracted_data={}
 
-    return data
+    fetched_data[0]=fetched_data[0]['data']['response']['body']['items']['item'][0]
+    detail_0_extract_list=['tel','homepage','overview']
+    for i in detail_0_extract_list:
+        if(fetched_data[0][i]):
+            extracted_data[i]=fetched_data[0][i]
+
+    fetched_data[1]=fetched_data[1]['data']['response']['body']['items']['item'][0]
+    detail_1_extract_list=['relaAcdntRiskMtr',"acmpyTypeCd","relaPosesFclty","relaFrnshPrdlst","etcAcmpyInfo","relaPurcPrdlst","acmpyPsblCpam","relaRntlPrdlst","acmpyNeedMtr"]
+    for i in detail_1_extract_list:
+        if(fetched_data[1][i]):
+            if('pet_detail' not in extracted_data.keys()):
+                extracted_data['pet_detail']=str(fetched_data[1][i])
+            else:
+                extracted_data['pet_detail']+=', '+str(fetched_data[1][i])
+
+    return extracted_data
