470501d20f165a830329cbde7309315ba3cf5764
MovingJu <baramishe@gmail.com>
2025-09-14 18:10:59 +0900

    upgrade total_travel_plan.py

--- routes/total_travel_plan.py
+++ routes/total_travel_plan.py
@@ -2,10 +2,20 @@ from fastapi import APIRouter, encoders
 import json, pandas as pd
 import modules, routes
 
+
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import math, json
+
+
 router = APIRouter(
     prefix="/calendar",
     tags=["Pick and Go Calendar service"]
 )
+
+def distance(center: tuple[float, float], arr: np.ndarray):
+    return np.sqrt((arr[:, 0] - center[0])**2 + (arr[:, 1] - center[1])**2)
 
 @router.post("/")
 async def post_calendar(item: modules.CalendarData, date: int = 3, food_day: int = 3, tour_day: int = 3):
@@ -15,36 +25,93 @@ async def post_calendar(item: modules.CalendarData, date: int = 3, food_day: int
             str(cached_tours[cached_tours.iloc[:, 0] == item.user_info.user_id].iloc[0, 1])
         )
 
-    tour = (item.selectedTour.items + cached_tours)[:400]
+    tour: list[modules.schema.TourItem | dict[str, str]] = (item.selectedTour.items + cached_tours)[:100]
 
-    food = await routes.post_food_list(item, 100000)
+    food1 = await routes.post_food_list(item, 100)
     hotel = await routes.post_hotel_list(item, 10)
 
-    food = food["data"]
+    food: list[dict[str, str]] = food1["data"] # type: ignore
     hotel = hotel["data"]
+    
+    gps = []
+    for idx, val in enumerate(tour):
+        if not isinstance(val, dict):
+            val = val.model_dump()
+        if float(val["mapx"]) < 120 or float(val["mapx"]) > 130:
+            continue
+        if float(val["mapy"]) < 30 or float(val["mapy"]) > 40:
+            continue
+
+        gps.append(
+            (
+                float(val["mapx"]), float(val["mapy"]), idx
+            )
+        )
+
+    gps = np.array(gps)
+
+    kmean = KMeans(
+        n_clusters=date, 
+        random_state=42
+    ).fit(gps)
 
 
-    # temp = encoders.jsonable_encoder({
-    #     "food" : food,
-    #     "tour" : tour,
-    #     "hotel" : hotel
-    # })
-    # with open("./result_sample.json", "w") as file:
-    #     file.write(
-    #         json.dumps(
-    #             temp,
-    #             indent=4,
-    #             ensure_ascii=False
-    #         )
-    #     )
+    labeled_gps = [[] for _ in range(date)]
+    for idx, label in enumerate(kmean.labels_):
+        labeled_gps[label].append(gps[idx])
 
-    # print(f"len food : {len(food)}, len tour : {len(tour)}")
+    labeled_gps = [
+        np.array(labeled_gps[i])
+        for i in range(date)
+    ]
+    
+    centerized_labeled_gps = []
+    for idx, elem in enumerate(labeled_gps):
+        temp = elem[np.argsort(distance(kmean.cluster_centers_[idx], elem))][:tour_day]
+        centerized_labeled_gps.append(temp)
 
+    modified_tour = []
+    for sub_list in centerized_labeled_gps:
+        temp = []
+        for elem in sub_list:
+            temp.append(
+                tour[int(elem[2])]
+            )
+        modified_tour.append(temp)
+
+
+    gps = []
+    for idx, val in enumerate(food):
+        if float(val["mapx"]) < 120 or float(val["mapx"]) > 130:
+            continue
+        if float(val["mapy"]) < 30 or float(val["mapy"]) > 40:
+            continue
+
+        gps.append(
+            (
+                float(val["mapx"]), float(val["mapy"]), idx
+            )
+        )
+    gps = np.array(gps)
+
+    centerized_gps = []
+    for idx in range(date):
+        temp = gps[np.argsort(distance(kmean.cluster_centers_[idx], gps))][:tour_day]
+        centerized_gps.append(temp)
+
+    modified_food = []
+    for sub_list in centerized_gps:
+        temp = []
+        for elem in sub_list:
+            temp.append(
+                tour[int(elem[2])]
+            )
+        modified_food.append(temp)
 
     schedules = [
         {
-            "food" : food[i * food_day : (i + 1) * food_day],
-            "tour_list" : tour[i * tour_day : (i + 1) * tour_day]
+            "food" : modified_food[i],
+            "tour_list" : modified_tour[i]
         }
         for i in range(date)
     ]
